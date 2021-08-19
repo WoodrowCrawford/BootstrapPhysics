23cd1e49d915e173cddf85076f6d33772ba59e36
Woodrow Crawford <woodrowcrawford03@gmail.com>
2021-08-19 12:59:36 -0500

    Started on Collision detection

--- PhysicsForGames/PhysicsProjects/PhysicsScene.h
+++ PhysicsForGames/PhysicsProjects/PhysicsScene.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "glm/vec2.hpp"
 #include <set>
+#include "Sphere.h"
 
 class PhysicsObject;
 
@@ -24,6 +25,8 @@ public:
 
 	void setTimeStep(const float timeStep) { m_timeStep = timeStep; }
 	float getTimeStep() const { return m_timeStep; }
+
+	static bool sphereToSphere(Sphere* sphere1, Sphere* sphere2);
 
 private:
 	glm::vec2 m_gravity;
