17a00f1e0ff3991c2f3dc08f845c035404281de8
Woodrow Crawford <woodrowcrawford03@gmail.com>
2021-08-24 15:15:16 -0500

    successfully created a collision resolution

--- PhysicsForGames/PhysicsProjects/PhysicsGame.cpp
+++ PhysicsForGames/PhysicsProjects/PhysicsGame.cpp
@@ -22,18 +22,18 @@ bool PhysicsGame::startup()
 	m_scene->setTimeStep(0.01f);
 
 	//Sets the gravity in the scene
-	m_scene->setGravity(glm::vec2{ 0.0f, -10.0f });
+	m_scene->setGravity(glm::vec2{ 0.0f, 0.0f });
 	
 
 	//Creates the balls
-	Sphere* ball1 = new Sphere(glm::vec2(-25, 0), glm::vec2(), 2, 10, glm::vec4(2.8f, 2.0f, 5.0f, 1.0f));
+	Sphere* ball1 = new Sphere(glm::vec2(-25, 0), glm::vec2(), 100, 10, glm::vec4(2.8f, 2.0f, 5.0f, 1.0f));
 	Sphere* ball2 = new Sphere(glm::vec2(25, 0), glm::vec2(), 2, 10, glm::vec4(1.9f, 0.0f, 0.0f, 1.9f));
 
 	//Creates the ground
 	Plane* ground = new Plane(glm::vec2(0.0f, 1.0f), -50, glm::vec4(1.0f, 0.3f, 0.1f, 1.0f));
 
 	//Applies forces
-	  //ball1->applyForce(glm::vec2(21.0f, 0.0f));
+	  ball1->applyForce(glm::vec2(21.0f, 0.0f));
 	  //ball2->applyForce(glm::vec2(-21.0f, 0));
 
 
--- PhysicsForGames/PhysicsProjects/RigidBody.h
+++ PhysicsForGames/PhysicsProjects/RigidBody.h
@@ -13,6 +13,7 @@ public:
 
 	void applyForce(glm::vec2 force);
 	void applyForceToOther(RigidBody* other, glm::vec2 force);
+	void resolveCollision(RigidBody* other);
 
 	glm::vec2 getPosition() { return m_position; }
 	glm::vec2 getVelocity() { return m_velocity; }
