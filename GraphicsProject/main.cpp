42aeddbcfc16f785bfacbf09a0636262b95d6d4f
Woodrow Crawford <woodrowcrawford03@gmail.com>
2021-09-28 12:22:18 -0500

    Added OpenGL and other dependencies

--- GraphicsProject/main.cpp
+++ GraphicsProject/main.cpp
@@ -1,14 +1,45 @@
 #include "gl_core_4_4.h"
 #include "GLFW/glfw3.h"
+#include <stdio.h>
 
 
 int main()
 {
+	//Initialize
 	if (!glfwInit())
 	{
 		return -1;
 	}
 	
+	//Create a window
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Test Window", nullptr, nullptr);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+
+
+
+	//Load OpenGL
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED)
+	{
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -3;
+	}
+	int majorVersion = ogl_GetMajorVersion();
+	int minorVersion = ogl_GetMinorVersion();
+	printf("OpenGL version %i.%i\n", majorVersion, minorVersion);
+
+
+
+	//Clean up and exit
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 
 }
